edc8c65c705e5743352edb26d218b07f958cbf30
ShotaIuchi <shotaiuchi.develop@gmail.com>
2015-11-11 13:20:03 +0900

    [emacs]encode
    * Encode modification

--- .emacs.d/init-lisp/init-encode.el
+++ .emacs.d/init-lisp/init-encode.el
@@ -1,3 +1,15 @@
+(setq default-input-method "W32-IME")
+(setq-default w32-ime-mode-line-state-indicator "[--]")
+(w32-ime-initialize)
+(setq w32-ime-mode-line-state-indicator-list '("[--]" "[あ]" "[--]"))
+(global-set-key [M-kanji] 'ignore)
+
+(set-language-environment  'utf-8)
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(setq default-file-name-coding-system 'shift_jis)
+
 ;; フォント
 ;; abcdefghijklmnopqrstuvwxyz
 ;; ABCDEFGHIJKLMNOPQRSTUVWXYZ
@@ -12,28 +24,23 @@
 ;; │　　　　　　　　　　　　　罫線                            │
 ;; └─────────────────────────────┘
 ;;
-
-;; 日本語入力
-(setq default-input-method "W32-IME")
-(w32-ime-initialize)
-
-(setq-default w32-ime-mode-line-state-indicator "[--]")
-(setq w32-ime-mode-line-state-indicator-list '("[--]" "[あ]" "[--]"))
-
-(set-cursor-color "red")
-(add-hook 'input-method-activate-hook
-          (lambda() (set-cursor-color "green")))
-(add-hook 'input-method-inactivate-hook
-          (lambda() (set-cursor-color "red")))
-
-(setq w32-ime-buffer-switch-p nil)
+(set-face-attribute 'default nil :family "Consolas" :height 100)
+(set-fontset-font (frame-parameter nil 'font)
+                  'japanese-jisx0208
+                  (font-spec :family "ＭＳ ゴシック" :size 14))
+;(set-face-attribute 'default nil :family "Consolas")
+;(setq face-font-rescale-alist '("ＭＳ ゴシック"))
 
 
-;; 文字コード
-(set-language-environment  'utf-8)
-(prefer-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(setq default-file-name-coding-system 'shift_jis) ; dired文字化け対策
+;(set-language-environment 'Japanese)
+;(set-terminal-coding-system 'utf-8)
+;(set-keyboard-coding-system 'utf-8)
+;(set-buffer-file-coding-system 'utf-8-unix)
+;(setq default-buffer-file-coding-system 'utf-8)
+;(prefer-coding-system 'utf-8)
+;(set-default-coding-systems 'utf-8)
+;(setq file-name-coding-system 'utf-8)
+;(set-clipboard-coding-system 'utf-8)
+;(setq default-input-method 'japanese-anthy)
 
 (provide 'init-encode)
