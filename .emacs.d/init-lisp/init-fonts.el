f96cad35e79edf805bc627ba46fa91ab95cc502d
ShotaIuchi <shotaiuchi.develop@gmail.com>
2015-11-11 13:22:02 +0900

    [emacs]font
    * Font modefication

--- .emacs.d/init-lisp/init-fonts.el
+++ .emacs.d/init-lisp/init-fonts.el
@@ -1,26 +1,12 @@
-;; フォント
-;; abcdefghijklmnopqrstuvwxyz
-;; ABCDEFGHIJKLMNOPQRSTUVWXYZ
-;; `1234567890-=\[];',./
-;; ~!@#$%^&*()_+|{}:"<>?
-;;
-;; 壱弐参四五壱弐参四五壱弐参四五壱弐参四五壱弐参四五壱弐参四五
-;; 123456789012345678901234567890123456789012345678901234567890
-;; ABCdeＡＢＣｄｅ
-;;
-;; ┌─────────────────────────────┐
-;; │　　　　　　　　　　　　　罫線                            │
-;; └─────────────────────────────┘
-;;
+(set-face-attribute
+ 'default nil
+ :family "Consolas"
+ :height 90)
 
-(set-face-attribute 'default nil :family "Consolas")
-(set-fontset-font (frame-parameter nil 'font)
-                  'japanese-jisx0208
-                  '("ＭＳ ゴシック" . "unicode-bmp")
-                  )
-(set-fontset-font (frame-parameter nil 'font)
-                  'katakana-jisx0201
-                  '("ＭＳ ゴシック" . "unicode-bmp")
-                  )
+;(set-fontset-font nil 'japanese-jisx0208
+;                  (font-spec :family "Consolas"))
+;
+;(setq face-font-rescale-alist
+;      '((".*Consolas.*" . 1.0)))
 
 (provide 'init-fonts)
