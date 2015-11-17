735a75f313ae26f52d1cafafe3453e0cf79c5009
ShotaIuchi <shotaiuchi.develop@gmail.com>
2015-11-17 12:26:18 +0900

    [script]setup.sh (Refactoring)
    * Function of processing

--- setup.sh
+++ setup.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
+#
+# variable
+PATH_ROOT=~
+PATH_DOTFILES=~/dotfiles
+
+#
+# link
+function initLink()
+{
+    src=${PATH_DOTFILES}/${1}
+    dst=${PATH_ROOT}/${1}
+    if [ -f ${dst} ] || [ -d ${dst} ]; then
+        if [ ! -h ${dst} ]; then
+            mv ${dst} ${dst}.local
+            ln -s ${src} ${dst}
+        fi
+    else
+        ln -s ${src} ${dst}
+    fi
+}
+initLink '.bashrc'
+initLink '.emacs.d'
+initLink '.gitconfig'
+initLink '.vimrc'
