5ee1d271595c3e47f865cdd4f26cd01ac711eef2
ShotaIuchi <shotaiuchi.develop@gmail.com>
2015-11-20 08:12:46 +0900

    [emacs]keybind (helm)
    * keybind
    * rename (helm -> anything)

--- .emacs.d/sub-lisp/sub-anything.el
+++ .emacs.d/sub-lisp/sub-anything.el
@@ -5,12 +5,12 @@
 (require 'helm-c-moccur)
 ;(require 'helm-descbinds)
 
-(global-set-key (kbd "C-x C-b") 'helm-mini)
-(global-set-key (kbd "C-x b") 'buffer-menu)
-(global-set-key (kbd "C-c i") 'helm-imenu)
-(global-set-key (kbd "C-c o") 'helm-occur)
-(global-set-key (kbd "C-c s") 'helm-ag)
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-unset-key (kbd "C-u"))
+(global-set-key (kbd "C-u C-u") 'helm-mini)
+(global-set-key (kbd "C-u C-i") 'helm-imenu)
+(global-set-key (kbd "C-u C-o") 'helm-occur)
+(global-set-key (kbd "C-u C-g") 'helm-ag)
+(global-set-key (kbd "C-u C-y") 'helm-show-kill-ring)
 
 ;; gtags
 ;(global-set-key (kbd "C-c p") 'helm-gtags-find-pattern)
@@ -24,5 +24,5 @@
 (setq helm-c-moccur-enable-initial-pattern t)
 (setq helm-c-moccur-enable-auto-look-flag t)
 
-(provide 'sub-helm)
+(provide 'sub-anything)
 
