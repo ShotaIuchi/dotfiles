d9f6e6925f50d3aa78c1701af87ae3c0195be493
ShotaIuchi <shotaiuchi.develop@gmail.com>
2015-11-28 01:09:01 +0900

    [emacs]gui
    * Switch gui code

--- .emacs.d/sub-lisp/sub-frame.el
+++ .emacs.d/sub-lisp/sub-frame.el
@@ -1,9 +1,11 @@
-(when (eq system-type 'windows-nt)
-  (tool-bar-mode 0)
-  (menu-bar-mode 0)
-  (global-linum-mode 0)
-  (scroll-bar-mode 0)
-  (line-number-mode t)
-  (column-number-mode t))
+(if window-system
+    (progn
+      (tool-bar-mode 0)
+      (menu-bar-mode 0)
+      (global-linum-mode 0)
+      (scroll-bar-mode 0)
+      (line-number-mode t)
+      (column-number-mode t))
+  )
 
 (provide 'sub-frame)
