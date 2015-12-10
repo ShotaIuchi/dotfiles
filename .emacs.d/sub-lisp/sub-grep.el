e1b314a973b05b0947bbfedd42d68c25b74a0721
ShotaIuchi <shotaiuchi.develop@gmail.com>
2015-12-10 19:00:38 +0900

    [emacs]delete:find,tags
    * Refactoring >>> find,tags -> grep

--- .emacs.d/sub-lisp/sub-grep.el
+++ .emacs.d/sub-lisp/sub-grep.el
@@ -1,8 +1,27 @@
+;; ========================================================================
+;;  grep
+;; ========================================================================
 (require 'wgrep nil t)
-
 (setq null-device "/dev/null")
-
 (global-set-key (kbd "C-x C-g") 'rgrep)
 
-(provide 'sub-grep)
 
+;; ========================================================================
+;;  tags
+;; ========================================================================
+(when (require 'gtags nil t)
+  (setq gtags-mode-hook
+        '(lambda ()
+           (local-set-key "\M-t" 'gtags-find-tag)
+           (local-set-key "\M-r" 'gtags-find-rtag)
+           (local-set-key "\M-s" 'gtags-find-symbol)
+           (local-set-key "\M-p" 'gtags-find-pattern)
+           (local-set-key "\M-a" 'gtags-find-file)
+           (local-set-key "\C-t" 'gtags-pop-stack)
+           ))
+  (add-hook 'c-mode-hook (lambda () (gtags-mode 1)))
+  (add-hook 'c++-mode-hook (lambda () (gtags-mode 1)))
+  (add-hook 'java-mode-hook	(lambda () (gtags-mode 1)))
+  )
+
+(provide 'sub-grep)
