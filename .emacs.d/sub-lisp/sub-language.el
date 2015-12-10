3e19a54e54c0e91931ded9d1bb69a97d81f9a8b8
ShotaIuchi <shotaiuchi.develop@gmail.com>
2015-12-11 08:33:01 +0900

    [emacs]language:c,C++
    * Delete sytle (google-c-style.el)

--- .emacs.d/sub-lisp/sub-language.el
+++ .emacs.d/sub-lisp/sub-language.el
@@ -2,8 +2,15 @@
 ;;  C,C++
 ;; ========================================================================
 (defun language-mode-hook ()
-  (require 'google-c-style)
-  (google-set-c-style)
+  (c-set-style "linux")
+  (setq c-basic-offset 4)
+  (setq c-tab-always-indent nil)
+  (setq c-auto-newline t)
+  (setq c-hungry-delete-key t)
+  (setq indent-tabs-mode nil)
+
+  (require 'rainbow-delimiters)
+  (rainbow-delimiters-mode)
   )
 (add-hook 'c-mode-hook 'language-mode-hook)
 (add-hook 'c++-mode-hook 'language-mode-hook)
