7ffb1e0bb0c69bda3c2511321d96ceb2bbd65b94
ShotaIuchi <shotaiuchi.develop@gmail.com>
2015-12-14 08:25:45 +0900

    [emacs]language:C,C++
    * refactoring

--- .emacs.d/sub-lisp/sub-language.el
+++ .emacs.d/sub-lisp/sub-language.el
@@ -1,19 +1,16 @@
 ;; ========================================================================
 ;;  C,C++
 ;; ========================================================================
-(defun language-mode-hook ()
+(defun c-mode-hook-impl ()
   (c-set-style "linux")
   (setq c-basic-offset 4)
   (setq c-tab-always-indent nil)
   (setq c-auto-newline t)
   (setq c-hungry-delete-key t)
-  (setq indent-tabs-mode nil)
+  (setq indent-tabs-mode nil))
+(add-hook 'c-mode-hook 'c-mode-hook-impl)
+(add-hook 'c++-mode-hook 'c-mode-hook-impl)
 
-  (require 'rainbow-delimiters)
-  (rainbow-delimiters-mode)
-  )
-(add-hook 'c-mode-hook 'language-mode-hook)
-(add-hook 'c++-mode-hook 'language-mode-hook)
 
 (provide 'sub-language)
 
