d78d75664cbfa98153307d6c6dd79968b59e21dd
ShotaIuchi <shotaiuchi.develop@gmail.com>
2016-07-06 17:23:22 +0900

    [emacs]font:windows
    * MeiryoKe_Gothic

--- .emacs.d/sub-lisp/sub-fonts.el
+++ .emacs.d/sub-lisp/sub-fonts.el
@@ -15,7 +15,9 @@
 ;; └─────────────────────────────┘
 ;;
 
+;; ========================================================================
 ;; japanese
+;; ========================================================================
 (set-language-environment  'utf-8)
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
@@ -23,21 +25,39 @@
 (setq default-file-name-coding-system 'shift_jis)
 
 
+;; ========================================================================
+;;  util
+;; ========================================================================
+(defun is-font-exist (font-name)
+  (if (null (x-list-fonts font-name)) nil t))
+
+
+;; ========================================================================
 ;; windows
+;; ========================================================================
 (when (eq system-type 'windows-nt)
-  (set-face-attribute 'default nil :family "Consolas" :height 104)
-  (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "メイリオ"))
-  (setq face-font-rescale-alist '(("メイリオ" . 1.08))))
+  (if (is-font-exist "MeiryoKe_Gothic")
+      (progn
+        (set-face-attribute 'default nil :family "MeiryoKe_Gothic" :height 90)
+        (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "MeiryoKe_Gothic"))
+        (setq face-font-rescale-alist '(("MeiryoKe_Gothic" . 1.00))))
+    (set-face-attribute 'default nil :family "Consolas" :height 104)
+    (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "メイリオ"))
+    (setq face-font-rescale-alist '(("メイリオ" . 1.08)))))
 
 
+;; ========================================================================
 ;; mac
+;; ========================================================================
 (when (eq system-type 'darwin)
   (set-face-attribute 'default nil :family "monaco" :height 104)
   (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Hiragino Kaku Gothic ProN"))
   (setq face-font-rescale-alist '(("Hiragino Kaku Gothic ProN" . 1.20))))
 
 
+;; ========================================================================
 ;; linux
+;; ========================================================================
 (when (eq system-type 'gnu/linux)
   )
 
