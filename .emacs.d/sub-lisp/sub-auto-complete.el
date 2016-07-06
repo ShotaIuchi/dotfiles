ba0c9c098ecb9ae9bd207af1cb89835c2c611052
ShotaIuchi <shotaiuchi.develop@gmail.com>
2016-07-06 16:39:05 +0900

    [emacs]auto-complete
    * Key bindings of input candidate.

--- .emacs.d/sub-lisp/sub-auto-complete.el
+++ .emacs.d/sub-lisp/sub-auto-complete.el
@@ -3,8 +3,9 @@
 ;(global-auto-complete-mode t)
 (ac-config-default)
 (define-key ac-completing-map (kbd "TAB") 'auto-complete)
-;(define-key ac-completing-map (kbd "C-n") 'ac-next)
-;(define-key ac-completing-map (kbd "C-p") 'ac-previous)
+(define-key ac-completing-map (kbd "C-n") 'ac-next)
+(define-key ac-completing-map (kbd "C-p") 'ac-previous)
+
 
 (provide 'sub-auto-complete)
 
