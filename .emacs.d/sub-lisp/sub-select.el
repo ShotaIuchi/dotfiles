f38e4ae9b31ec1b5837a23f3986ce300ac656e8d
ShotaIuchi <shotaiuchi.develop@gmail.com>
2016-07-13 00:46:31 +0900

    [emacs]swap-line
    * line swap

--- .emacs.d/sub-lisp/sub-select.el
+++ .emacs.d/sub-lisp/sub-select.el
@@ -34,4 +34,19 @@
   )
 
 
+;;----------------------------------------------------------------------------
+;; swap line
+;;----------------------------------------------------------------------------
+(defun move-line-up ()
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2))
+(defun move-line-down ()
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1))
+(global-set-key (kbd "M-<up>") 'move-line-up)
+(global-set-key (kbd "M-<down>") 'move-line-down)
+
 (provide 'sub-select)
