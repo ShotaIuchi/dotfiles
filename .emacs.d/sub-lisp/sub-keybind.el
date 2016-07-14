acae7a467f964c0afc6bf7cad60e7cbeef10e95f
ShotaIuchi <shotaiuchi.develop@gmail.com>
2016-07-14 20:09:14 +0900

    [emacs]go-top
    * C-a

--- .emacs.d/sub-lisp/sub-keybind.el
+++ .emacs.d/sub-lisp/sub-keybind.el
@@ -10,6 +10,22 @@
 (global-set-key (kbd "M-k") 'kill-buffer)       ; Cut the cursor below.
 
 ;;----------------------------------------------------------------------------
+;; go-to indented line
+;;----------------------------------------------------------------------------
+(global-set-key "\C-a" 'beggining-of-indented-line)
+(defun beggining-of-indented-line (current-point)
+  "[top]<-->[indent]"
+  (interactive "d")
+  (if (string-match
+       "^[ \t]+$"
+       (save-excursion
+         (buffer-substring-no-properties
+          (progn (beginning-of-line) (point))
+          current-point)))
+      (beginning-of-line)
+    (back-to-indentation)))
+
+;;----------------------------------------------------------------------------
 ;; mouse
 ;;----------------------------------------------------------------------------
 (xterm-mouse-mode t)
