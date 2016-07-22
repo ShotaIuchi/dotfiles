f20dd8ed70261eee0a9dbb45a212ccc4ed815e1f
ShotaIuchi <shotaiuchi.develop@gmail.com>
2016-07-22 20:47:27 +0900

    [emacs]backup:auto-save
    * backup/

--- .emacs.d/sub-lisp/sub-backup.el
+++ .emacs.d/sub-lisp/sub-backup.el
@@ -1,14 +1,25 @@
 ;; ========================================================================
+;;  path
+;; ========================================================================
+(defvar path-backup-dir
+  (concat
+   (expand-file-name user-emacs-directory) "backup/"))
+
+
+;; ========================================================================
 ;;  backup
 ;; ========================================================================
-(setq make-backup-files nil)
+(setq make-backup-files t)
+(setq backup-directory-alist '(("" . path-backup-dir)))
 
 
 ;; ========================================================================
 ;;  auto-save
 ;; ========================================================================
-(setq auto-save-default nil)
-(setq auto-save-list-file-prefix nil)
+(setq auto-save-default t)
+(setq auto-save-list-file-prefix t)
+(setq auto-save-file-name-transforms
+      `((".*", (expand-file-name path-backup-dir) t)))
 
 
 (provide 'sub-backup)
