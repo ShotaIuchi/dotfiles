9bd646b777b948f71f9475cc4f2af4e4cbbe15d1
ShotaIuchi <shotaiuchi.develop@gmail.com>
2016-07-22 19:56:27 +0900

    [emacs]helm:delay
    * 1.2 --> 0.2

--- .emacs.d/sub-lisp/sub-helm-init.el
+++ .emacs.d/sub-lisp/sub-helm-init.el
@@ -5,8 +5,8 @@
 (if (not (require 'helm-config nil t))
     (message "!!! WORNING !!! | require : helm-config")
   (require 'helm)
-  (setq helm-idle-delay 1.2)
-  (setq helm-input-idle-delay 1.2)
+  (setq helm-idle-delay 0.2)
+  (setq helm-input-idle-delay 0.2)
   (defconst flag-helm-init t))
 
 
