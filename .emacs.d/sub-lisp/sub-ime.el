81133bb4ac8ff65f525375cb11ba8dec3360aca3
ShotaIuchi <shotaiuchi.develop@gmail.com>
2016-07-27 14:17:38 +0900

    [emacs]ime:skk-keybind
    * C-x j    skk-auto-file-mode --> skk-mode
    * C-x C-j  skk-auto-file-mode --> skk-mode
    * C-x C-\\ ---                --> skk-auto-fill-mode

--- .emacs.d/sub-lisp/sub-ime.el
+++ .emacs.d/sub-lisp/sub-ime.el
@@ -3,8 +3,9 @@
 ;; ========================================================================
 (if (not (require 'skk nil t))
     (message "!!! WORNING !!! | require : skk")
-  (global-set-key (kbd "C-x j") 'skk-auto-fill-mode)
-  (global-set-key (kbd "C-x C-j") 'skk-auto-fill-mode)
+  (global-set-key (kbd "C-x j") 'skk-mode)
+  (global-set-key (kbd "C-x C-j") 'skk-mode)
+  (global-set-key (kbd "C-x C-\\") 'skk-auto-fill-mode)
   (setq default-input-method "japanese-skk")
   (unless (require 'skk-study nil t)
     (message "!!! WORNING !!! | require : skk-study")))
