b2207cb5abe98a78d2c4fcc5b156ec358dcd01c1
ShotaIuchi <shotaiuchi.develop@gmail.com>
2018-05-07 22:38:51 +0900

    [git][bugfix]setupLinux.sh
    setupLink => SetupLink

--- setupLinux.sh
+++ setupLinux.sh
@@ -7,7 +7,7 @@ THIS_PATH=~/dotfiles
 SetupLink ${TARGET_PATH} ${THIS_PATH} .bashrc
 SetupLink ${TARGET_PATH} ${THIS_PATH} .script.d
 SetupLink ${TARGET_PATH} ${THIS_PATH} .gitconfig
-setupLink ${TARGET_PATH} ${THIS_PATH} .emacs.d
+SetupLink ${TARGET_PATH} ${THIS_PATH} .emacs.d
 SetupLink ${TARGET_PATH} ${THIS_PATH} .vim
 SetupLink ${TARGET_PATH} ${THIS_PATH} .vimrc
 SetupLink ${TARGET_PATH} ${THIS_PATH} .uncrustify.cfg
