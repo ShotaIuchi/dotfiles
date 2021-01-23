931e6493927714713644d059b7a8ff0ddd31bf2d
Shota Iuchi <shotaiuchi.develop@gmail.com>
2021-01-23 21:57:16 +0900

    [vscode]keybindings.json

--- setupMac.sh
+++ setupMac.sh
@@ -10,3 +10,5 @@ ln -s $(PWD)/.emacs.d ~/
 ln -s $(PWD)/.vim ~/
 ln -s $(PWD)/.vimrc ~/
 ln -s $(PWD)/.uncrustify.cfg ~/
+
+ln $(PWD)/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
